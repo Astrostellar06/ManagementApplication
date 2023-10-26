572f98082aa7112975670a7d9d2927e051d91df7
Astrostellar <laubertie.clement@gmail.com>
2023-10-26 10:12:49 +0200

    Added Order module, improved lisibility and maintenance with better files tree

---
+++ WpfApp1/Objects/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfApp1;
+
+public class Order
+{
+    public List<Pizza> pizzas { get; set; }
+    public List<Drink> drinks { get; set; } = new List<Drink>();
+    public int orderNumber { get; set; }
+    public DateTime orderTime { get; set; }
+    public DateTime orderDate { get; set; }
+    public string customerName { get; set; }
+    public string clerkName { get; set; }
+    public Address address { get; set; }
+    
+    public Order(List<Pizza> pizzas, List<Drink> drinks, int orderNumber, DateTime orderTime, DateTime orderDate, string customerName, string clerkName, Address address)
+    {
+        this.pizzas = pizzas;
+        this.drinks = drinks;
+        this.orderNumber = orderNumber;
+        this.orderTime = orderTime;
+        this.orderDate = orderDate;
+        this.customerName = customerName;
+        this.clerkName = clerkName;
+        this.address = address;
+    }
+    
+    public Order(List<Pizza> pizzas, int orderNumber, DateTime orderTime, DateTime orderDate, string customerName, string clerkName, Address address)
+    {
+        this.pizzas = pizzas;
+        this.orderNumber = orderNumber;
+        this.orderTime = orderTime;
+        this.orderDate = orderDate;
+        this.customerName = customerName;
+        this.clerkName = clerkName;
+        this.address = address;
+    }
+}
